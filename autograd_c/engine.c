fee19b3a1098c526e3c8f7b154b947266b49ee92
Harikrishna-AL <amenu4aie21034@am.students.amrita.edu>
2025-02-25 12:06:02 +0530

    add: backward function and topo_sort func

--- autograd_c/engine.c
+++ autograd_c/engine.c
@@ -1,19 +1,74 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-// started creating an autograd engine finally!
+//------------------------------
+// Autograd engine definitions
+//------------------------------
+
 typedef struct Value Value;
 
-struct Value{
+struct Value {
     float data;
     float grad;
     void (*_backward)(Value*);
-    Value** _prev;
+    Value** _prev;  
     char _op;
 };
 
+typedef struct {
+    Value** data;   
+    size_t size;     
+    size_t capacity; 
+} ValueArray;
+
+//------------------------------
+// Dynamic array helper functions
+//------------------------------
+
+void init_value_array(ValueArray* arr, size_t initial_capacity) {
+    arr->data = (Value**)malloc(initial_capacity * sizeof(Value*));
+    if (!arr->data) {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
+    arr->size = 0;
+    arr->capacity = initial_capacity;
+}
+
+void push_value_array(ValueArray* arr, Value* v) {
+    if (arr->size >= arr->capacity) {
+        arr->capacity *= 2;
+        Value** temp = (Value**)realloc(arr->data, arr->capacity * sizeof(Value*));
+        if (!temp) {
+            perror("realloc failed");
+            free(arr->data);
+            exit(EXIT_FAILURE);
+        }
+        arr->data = temp;
+    }
+    arr->data[arr->size++] = v;
+}
+
+bool is_in_array(ValueArray* arr, Value* v) {
+    for (size_t i = 0; i < arr->size; i++) {
+        if (arr->data[i] == v) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//------------------------------
+// Value creation and operations
+//------------------------------
+
 Value* create_value(float data) {
     Value* new_value = (Value*)malloc(sizeof(Value));
+    if (!new_value) {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
     new_value->data = data;
     new_value->grad = 0;
     new_value->_backward = NULL;
@@ -23,20 +78,28 @@ Value* create_value(float data) {
 }
 
 void add_backward(Value* self) {
+    if (self->_prev != NULL) {
         self->_prev[0]->grad += self->grad;
         self->_prev[1]->grad += self->grad;
+    }
 }
 
 void mul_backward(Value* self) {
+    if (self->_prev != NULL) {
         self->_prev[0]->grad += self->_prev[1]->data * self->grad;
         self->_prev[1]->grad += self->_prev[0]->data * self->grad;
+    }
 }
 
 Value* add(Value* v1, Value* v2) {
-    int data = v1->data + v2->data;
+    float data = v1->data + v2->data;
     Value* new_value = create_value(data);
 
-    new_value->_prev = (Value**)malloc(2*sizeof(Value*));
+    new_value->_prev = (Value**)malloc(2 * sizeof(Value*));
+    if (!new_value->_prev) {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
     new_value->_prev[0] = v1;
     new_value->_prev[1] = v2;
     new_value->_op = '+';
@@ -46,10 +109,14 @@ Value* add(Value* v1, Value* v2) {
 }
 
 Value* mul(Value* v1, Value* v2) {
-    int data = v1->data * v2->data;
+    float data = v1->data * v2->data;
     Value* new_value = create_value(data);
 
-    new_value->_prev = (Value**)malloc(2*sizeof(Value*));
+    new_value->_prev = (Value**)malloc(2 * sizeof(Value*));
+    if (!new_value->_prev) {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
     new_value->_prev[0] = v1;
     new_value->_prev[1] = v2;
     new_value->_op = '*';
@@ -58,60 +125,69 @@ Value* mul(Value* v1, Value* v2) {
     return new_value;
 }
 
-// void backward(Value* v){
-//     // topological sort of the children in the graph
-//     Value** topo[] = {};
-//     // make a visited set
-//     Value** visited[] = {};
+//------------------------------
+// Topological sort
+//------------------------------
 
-//     void build_top(Value* v); {
-//         if (v == NULL){
-//             return;
-//         }
-//         if (v in visited){
-//             return;
-//         }
-//         visited.push(v);
-//         for (int i = 0; i < v->_prev.length; i++){
-//             build_top(v->_prev[i]);
-//         }
-//         topo.push(v);
-//     }
+/*
+   The build_top function performs a depth-first search on the computation graph.
+   It uses the dynamic arrays "visited" to track nodes we have already seen and
+   "topo" to store the topological ordering.
+*/
+void build_top(Value* v, ValueArray* topo, ValueArray* visited) {
+    if (v == NULL)
+        return;
 
+    if (is_in_array(visited, v))
+        return;
+    
+    push_value_array(visited, v);
+    
+    if (v->_prev != NULL) {
+        build_top(v->_prev[0], topo, visited);
+        build_top(v->_prev[1], topo, visited);
+    }
+    push_value_array(topo, v);
+}
 
-// }
+ValueArray* topological_sort(Value* v) {
+    ValueArray* topo = (ValueArray*)malloc(sizeof(ValueArray));
+    ValueArray* visited = (ValueArray*)malloc(sizeof(ValueArray));
+    if (!topo || !visited) {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
+    init_value_array(topo, 10);
+    init_value_array(visited, 10);
 
-// test struct
-// int main(){
-//     Value* v1 = create_value(3);
-//     Value* v2 = create_value(4);
+    build_top(v, topo, visited);
 
-//     printf("v1.data: %f\n", v1->data);
-//     printf("v1.grad: %f\n", v1->grad);
+    free(visited->data);
+    free(visited);
+    return topo;
+}
 
-//     printf("v2.data: %f\n", v2->data);
-//     printf("v2.grad: %f\n", v2->grad);
+//------------------------------
+// Backward pass
+//------------------------------
 
-//     Value* v3 = add(v1, v2);
-//     Value* v4 = mul(v1, v3);
+/*
+   The backward function recursively calls _backward on each node.
+   (In the topologically sorted order, you typically iterate in reverse order.)
+*/
 
-//     printf("v3.data: %f\n", v3->data);
-//     printf("v4.data: %f\n", v4->data);
+void backward(Value* v) {
+    v->grad = 1.0;
 
-//     // backward(v3);
-//     // backward(v4);
+    ValueArray* topo = topological_sort(v);
 
-//     printf("Printing the gradients after backward pass\n");
-//     printf("v1.grad: %f\n", v1->grad);
-//     printf("v2.grad: %f\n", v2->grad);
-//     printf("v3.grad: %f\n", v3->grad);
+    for (int i = (int)topo->size - 1; i >= 0; i--) {
+        Value* node = topo->data[i];
+        if (node->_backward != NULL) {
+            node->_backward(node);
+        }
+    }
 
-//     free(v3->_prev);
-//     free(v4->_prev);
-//     free(v3);
-//     free(v4);
-//     free(v1);
-//     free(v2);
-
-//     return 0;
-// }
+    free(topo->data);
+    free(topo);
+}
--- autograd_c/nn.c
+++ autograd_c/nn.c
@@ -5,11 +5,25 @@ int main(){
     Value* v2 = create_value(4);
     Value* v3 = add(v1, v2);
     Value* v4 = mul(v1, v2);
+    Value* v5 = add(v3, v4);
 
     printf("v1.data: %f\n", v1->data);
     printf("v2.data: %f\n", v2->data);
     printf("v3.data: %f\n", v3->data);
     printf("v4.data: %f\n", v4->data);
+    printf("v5.data: %f\n", v5->data);
+
+    // for (int i = 0; i < 5; i++){
+    //     printf("%f ", topo_sort[i]->data);
+    // }
+    // printf("\n");
+
+    backward(v5);
+    printf("v1.grad: %f\n", v1->grad);
+    printf("v2.grad: %f\n", v2->grad);
+    printf("v3.grad: %f\n", v3->grad);
+    printf("v4.grad: %f\n", v4->grad);
+    printf("v5.grad: %f\n", v5->grad);
 
     free(v1);
     free(v2);
